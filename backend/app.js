3d51d6f20e81168a832c1417554ed40818326e02
dasyx <seanpnn00@gmail.com>
2021-03-01 00:08:03 +0100

    app.js + server.js + modele mongoose

---
+++ backend/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+// Importation du package pour extraire l'objet JSON de la demande
+const bodyParser = require('body-parser');
+// Importation du package mongoose pour accèder à la base de données
+const mongoose = require('mongoose');
+// Importation du modèle mongoose pour le schéma de sauces
+const Sauce = require('./models/sauce');
+
+const app = express();
+
+// Connection au cluster MongoDB
+mongoose.connect('mongodb+srv://dasyx:mzQyjOdjJNajPMqL@cluster0.ap1uo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// Grâce à ces headers, on pourra accèder notre API depuis n'importe quelle origine, et envoyer différents types de requêtes
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.post('/api/sauces', (req, res, next) => {
+    delete req.body._id;
+    const sauce = new Sauce({
+      ...req.body
+    });
+    sauce.save()
+      .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+});
+
+module.exports = app;
---
+++ backend/models/sauce.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+// Création d'un schéma de données strictes pour cadrer avec le modèle de sauces. Certains champs seront obligatoires.
+const sauceSchema = mongoose.Schema({
+  // Identifiant du créateur de la sauce  
+  userId: { type: String, required: true },
+  // Nom de la sauce
+  name: { type: String, required: true },
+  // Nom du créateur de la sauce
+  manufacturer: { type: String, required: true },
+  // Description de la sauce
+  description: { type: String, required: true },
+  // Ingrédient principal
+  mainPepper: { type: Number, required: true },
+  // Adresse de la photo de présentation
+  imageUrl: { type: String, required: true },
+  // Indice de puissance de la sauce
+  heat: { type: Number, required: true },
+  // Nombre de mentions j'aime de la sauce
+  likes: { type: Number, },
+  // Nombre de mentions je n'aime pas
+  dislikes: { type: Number, },
+  // Tableau d'utilisateurs ayant aimé la sauce
+  usersLiked: { type: [String] },
+  // Tableau d'utilisateurs n'ayant pas aimé la sauce
+  usersDisliked:{ type: [String] }
+});
+
+module.exports = mongoose.model('Sauce', sauceSchema);
